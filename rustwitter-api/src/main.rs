bae9bd8f4848f998cadef1b452f46d9611616a23
Dolpheyn <farissufyan99@gmail.com>
2020-05-21 22:30:46 +0800

    implement dotenv and thiserror

--- rustwitter-api/src/main.rs
+++ rustwitter-api/src/main.rs
@@ -1,16 +1,29 @@
-
-
-
-static IP: &str = "127.0.0.1";
-static PORT: &str = "8080";
+use dotenv;
+use std::{ env::var };
 
 #[async_std::main] 
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), Error> {
+    dotenv::dotenv().ok();
+    pretty_env_logger::init();
+
+    let db_url = var("DATABASE_URL").unwrap();
+    let app_url = var("APP_URL").unwrap();
+    let app_port = var("APP_PORT").unwrap();
+    dbg!(db_url);
 
     let mut app = tide::new();
     app.at("/").get(|_| async move { Ok("Hello, world!") });
 
-    println!("Listening at {}:{}", IP, PORT); 
-    app.listen(format!("{}:{}", IP, PORT)).await?;
+    app.listen(format!("{}:{}", app_url, app_port)).await?;
+
     Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error(transparent)]
+    DbError(#[from] sqlx::Error),
+
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
