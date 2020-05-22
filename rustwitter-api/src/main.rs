3f8386b589a2d822c51246d7e9abe89b639928bf
Dolpheyn <farissufyan99@gmail.com>
2020-05-22 17:00:06 +0800

    Remove custom error type. Using unwrap() instead

--- rustwitter-api/src/main.rs
+++ rustwitter-api/src/main.rs
@@ -4,15 +4,15 @@ use sqlx::{ Pool, PgPool, query };
 use tide::{ Server, Request };
 
 #[async_std::main] 
-async fn main() -> Result<(), Error> {
+async fn main() {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
-    let db_url = var("DATABASE_URL")?;
-    let app_url = var("APP_URL")?;
-    let app_port = var("APP_PORT")?;
+    let db_url = var("DATABASE_URL").unwrap();
+    let app_url = var("APP_URL").unwrap();
+    let app_port = var("APP_PORT").unwrap();
 
-    let db_pool: PgPool = Pool::new(&db_url).await?;
+    let db_pool: PgPool = Pool::new(&db_url).await.unwrap();
 
     let mut app: Server<State> = tide::with_state(State {
 	db_pool,
@@ -22,28 +22,14 @@ async fn main() -> Result<(), Error> {
 	let db_pool = &req.state().db_pool;
 	let row = query!("select 'nadia' as cutie")
 	    .fetch_one(db_pool)
-	    .await?;
+	    .await.unwrap();
 
 	Ok(format!("{}", row.cutie.unwrap()))
     });
-    app.listen(format!("{}:{}", app_url, app_port)).await?;
-
-    Ok(())
+    app.listen(format!("{}:{}", app_url, app_port)).await.unwrap();
 }
 
 #[derive(Debug)]
 struct State {
     db_pool: PgPool,
-}
-
-#[derive(thiserror::Error, Debug)]
-enum Error {
-    #[error(transparent)]
-    DbError(#[from] sqlx::Error),
-
-    #[error(transparent)]
-    IoError(#[from] std::io::Error),
-
-    #[error(transparent)]
-    VarError(#[from] std::env::VarError),
 }
