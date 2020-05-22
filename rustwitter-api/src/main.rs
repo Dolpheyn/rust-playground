f3fa9d2c168d302a75ba31b4d076dbf1c3522095
Dolpheyn <farissufyan99@gmail.com>
2020-05-22 16:40:07 +0800

    Add psql connection (using sqlx) to the server state.

--- rustwitter-api/src/main.rs
+++ rustwitter-api/src/main.rs
@@ -1,22 +1,39 @@
 use dotenv;
 use std::{ env::var };
+use sqlx::{ Pool, PgPool, query };
+use tide::{ Server, Request };
 
 #[async_std::main] 
 async fn main() -> Result<(), Error> {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
-    let db_url = var("DATABASE_URL").unwrap();
-    let app_url = var("APP_URL").unwrap();
-    let app_port = var("APP_PORT").unwrap();
-    dbg!(db_url);
+    let db_url = var("DATABASE_URL")?;
+    let app_url = var("APP_URL")?;
+    let app_port = var("APP_PORT")?;
 
-    let mut app = tide::new();
-    app.at("/").get(|_| async move { Ok("Hello, world!") });
+    let db_pool: PgPool = Pool::new(&db_url).await?;
 
+    let mut app: Server<State> = tide::with_state(State {
+	db_pool,
+    });
+
+    app.at("/").get(|req: Request<State>| async move {
+	let db_pool = &req.state().db_pool;
+	let row = query!("select 'nadia' as cutie")
+	    .fetch_one(db_pool)
+	    .await?;
+
+	Ok(format!("{}", row.cutie.unwrap()))
+    });
     app.listen(format!("{}:{}", app_url, app_port)).await?;
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct State {
+    db_pool: PgPool,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -26,4 +43,7 @@ enum Error {
 
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    VarError(#[from] std::env::VarError),
 }
