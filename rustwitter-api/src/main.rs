385cf0ff89e04478c57de06131db6d9e04555594
Dolpheyn <farissufyan99@gmail.com>
2020-05-26 17:15:49 +0800

    Move test to module tests

--- rustwitter-api/src/main.rs
+++ rustwitter-api/src/main.rs
@@ -5,6 +5,9 @@ use sqlx::{query, PgPool, Pool};
 use std::env::var;
 use tide::{http, Request, Response, Server};
 
+#[cfg(test)]
+mod tests;
+
 #[async_std::main]
 async fn main() {
     dotenv::dotenv().ok();
@@ -49,35 +52,4 @@ async fn server() -> Server<State> {
 #[derive(Debug)]
 struct State {
     db_pool: PgPool,
-}
-
- #[cfg(test)]
- mod test {
-    use super::server;
-    use super::json;
-
-    use http_types::{Method, Request, Url};
-    use http_service_mock::make_server;
-
-    #[async_std::test]
-    async fn index_test() {
-        let app = server().await;
-        let mut server = make_server(app).unwrap();
-
-        let req = Request::new(
-            Method::Get,
-            Url::parse("http://127.0.0.1:8000/").unwrap(),
-        );
-
-        let res = server.simulate(req).unwrap();
-
-        let expected_body = json!({
-            "status": "Ok",
-            "data": {
-                "cutie": "nadia",
-            },
-        });
-        assert_eq!(res.status(), 200);
-        assert_eq!(res.body_string().await.unwrap(), expected_body.to_string());
-    }
 }
