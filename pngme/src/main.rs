f69733d163b4de7fd9bef1852fb4ffe8c384d01a
Dolpheyn <farissufyan99@gmail.com>
2021-03-01 10:49:43 +0800

    main: Add StrError

--- pngme/src/main.rs
+++ pngme/src/main.rs
@@ -4,8 +4,22 @@ mod chunk_type;
 mod commands;
 mod png;
 
-pub type Error = Box<dyn std::error::Error>;
+use std::error;
+use std::fmt;
+
+pub type Error = Box<dyn error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub struct StrError<'a>(&'a str);
+impl<'a> error::Error for StrError<'a> {}
+
+impl<'a> fmt::Display for StrError<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Delegate to the Display impl for `&str`:
+        self.0.fmt(f)
+    }
+}
 
 fn main() -> Result<()> {
     todo!()
